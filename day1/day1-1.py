715149a1da31288d92c4ad248c15b078159a3216
Martin Karlsson <xenovoyance@gmail.com>
2019-12-02 11:18:04 +0100

    Less code

--- day1/day1-1.py
+++ day1/day1-1.py
@@ -1,35 +1,23 @@
 #!/usr/local/bin/python3
 
-run_env = "prod"  # test or prod
-
-if run_env == "test":
-    input = "input-test.txt"
-else:
-    input = "input.txt"
-
+# input = "input-test.txt"
+input = "input.txt"
 totalFuel1 = 0
 totalFuel2 = 0
 
 
 def fuelneeded(startingFuel):
-    return int((truncate((int(startingFuel)/3))-2))
+    return int(int(startingFuel)/3)-2
 
 
 def additionalfuelneeded(startingFuel):
     totalfuel = 0
-
     startingFuel = fuelneeded(startingFuel)
 
     while(startingFuel > 0):
-        securityswitch += 1
         totalfuel += startingFuel
         startingFuel = fuelneeded(startingFuel)
     return totalfuel
-
-
-def truncate(n, decimals=0):
-    multiplier = 10 ** decimals
-    return int(n * multiplier) / multiplier
 
 
 with open(input) as blockstream:
@@ -38,5 +26,4 @@ with open(input) as blockstream:
         totalFuel1 += newFuel
         totalFuel2 += newFuel + additionalfuelneeded(newFuel)
 
-print("P1: Total fuel:", totalFuel1)
-print("P2: Total fuel:", totalFuel2)
+print("P1:", totalFuel1, "P2:", totalFuel2)
